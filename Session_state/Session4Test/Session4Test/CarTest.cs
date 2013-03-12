8c7516513aca453f2e8dcaa328cce0a5fb2ed7a8
Zhang Yue <zhangyue02135@yahoo.com.cn>
2013-03-12 15:26:27 +0800

    Added Session_State.

---
+++ Session_state/Session4Test/Session4Test/CarTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Session4Test
+{
+    [TestClass]
+    public class CarTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
